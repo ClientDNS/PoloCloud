d732aa4cce9d12d9ad7acd522c2c414805b4c0b6
HttxMarco <mircolindenau04@gmail.com>
2022-01-24 15:02:41 +0100

    Default help command implementation

---
+++ cloudsystem-base/src/main/java/de/bytemc/cloud/command/impl/HelpCloudCommand.java
@@ -0,0 +1,25 @@
+package de.bytemc.cloud.command.impl;
+
+import de.bytemc.cloud.api.CloudAPI;
+import de.bytemc.cloud.api.command.CloudCommand;
+import de.bytemc.cloud.api.command.CommandManager;
+import de.bytemc.cloud.api.command.executor.ExecutorType;
+import de.bytemc.cloud.api.command.executor.ICommandSender;
+
+public class HelpCloudCommand extends CloudCommand {
+
+    public HelpCloudCommand() {
+        super("help", "All commands and help descriptions", ExecutorType.CONSOLE);
+    }
+
+    @Override
+    public void execute(ICommandSender sender, String[] args) {
+
+        CommandManager manager = CloudAPI.getInstance().getCommandManager();
+
+        CloudAPI.getInstance().getLoggerProvider().logMessage("All possible commands(§b" + manager.getCachedCloudCommands().size() + "§7):");
+        manager.getCachedCloudCommands().forEach(it -> CloudAPI.getInstance().getLoggerProvider().logMessage("§b" + it.getCommandName() + "§7 - " + it.getDescription()));
+    }
+
+
+}
