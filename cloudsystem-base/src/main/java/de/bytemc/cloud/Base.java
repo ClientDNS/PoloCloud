4d6684c50a1544841b858455be53198367749b7b
HttxMarco <mircolindenau04@gmail.com>
2022-01-24 11:11:37 +0100

    Implement bootstrap and logger informations

--- cloudsystem-base/src/main/java/de/bytemc/cloud/Base.java
+++ cloudsystem-base/src/main/java/de/bytemc/cloud/Base.java
@@ -3,6 +3,7 @@ package de.bytemc.cloud;
 import de.bytemc.cloud.api.CloudAPI;
 import de.bytemc.cloud.api.CloudAPITypes;
 import de.bytemc.cloud.api.groups.IGroupManager;
+import de.bytemc.cloud.api.logger.LogType;
 import de.bytemc.cloud.api.services.IServiceManager;
 import de.bytemc.cloud.command.DefaultCommandSender;
 import lombok.Getter;
@@ -14,6 +15,10 @@ public class Base extends CloudAPI {
 
     public Base() {
         super(CloudAPITypes.NODE);
+
+        getLoggerProvider().logMessage("§7Cloudsystem » §b@ByteMC §7| §7Developed by: §bHttpMarco §7| Date: §b19.01.2020", LogType.EMPTY);
+        getLoggerProvider().logMessage(" ", LogType.EMPTY);
+
 
         instance = this;
     }
---
+++ cloudsystem-base/src/main/java/de/bytemc/cloud/bootstrap/BaseBootstrap.java
@@ -0,0 +1,11 @@
+package de.bytemc.cloud.bootstrap;
+
+import de.bytemc.cloud.Base;
+
+public class BaseBootstrap {
+
+    public static void main(String[] args){
+        new Base();
+    }
+
+}
