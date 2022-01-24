efc7560f130b88783f1735969e3ba42fd7e9cb96
HttxMarco <mircolindenau04@gmail.com>
2022-01-24 15:03:43 +0100

    Add a simple shutdown command

---
+++ cloudsystem-base/src/main/java/de/bytemc/cloud/command/impl/ShutdownCloudCommand.java
@@ -0,0 +1,20 @@
+package de.bytemc.cloud.command.impl;
+
+import de.bytemc.cloud.Base;
+import de.bytemc.cloud.api.command.CloudCommand;
+import de.bytemc.cloud.api.command.executor.ExecutorType;
+import de.bytemc.cloud.api.command.executor.ICommandSender;
+
+public class ShutdownCloudCommand extends CloudCommand {
+
+    public ShutdownCloudCommand() {
+        super("stop","Stop the cloudsystem", ExecutorType.CONSOLE);
+    }
+
+    @Override
+    public void execute(ICommandSender sender, String[] args) {
+        ((Base) Base.getInstance()).onShutdown();
+    }
+
+
+}
