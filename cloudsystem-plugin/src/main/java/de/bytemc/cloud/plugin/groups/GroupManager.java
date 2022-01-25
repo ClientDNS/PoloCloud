f7ec27ec8c351a75a14d68e54041e2a80340c56a
HttxMarco <mircolindenau04@gmail.com>
2022-01-25 11:44:41 +0100

    Implement basic group and service manager for plugin side

---
+++ cloudsystem-plugin/src/main/java/de/bytemc/cloud/plugin/groups/GroupManager.java
@@ -0,0 +1,9 @@
+package de.bytemc.cloud.plugin.groups;
+
+import de.bytemc.cloud.api.groups.impl.AbstractGroupManager;
+
+public class GroupManager extends AbstractGroupManager {
+
+
+
+}
---
+++ cloudsystem-plugin/src/main/java/de/bytemc/cloud/plugin/services/ServiceManager.java
@@ -0,0 +1,9 @@
+package de.bytemc.cloud.plugin.services;
+
+import de.bytemc.cloud.api.services.impl.AbstractSimpleServiceManager;
+
+public class ServiceManager extends AbstractSimpleServiceManager {
+
+
+
+}
