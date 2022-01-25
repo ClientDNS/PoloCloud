c4818ea6186bbc30f9ca4875d3810f37f21914bc
HttxMarco <mircolindenau04@gmail.com>
2022-01-25 13:39:37 +0100

    Add connect parameters and logs

--- cloudsystem-plugin/src/main/java/de/bytemc/cloud/plugin/CloudPlugin.java
+++ cloudsystem-plugin/src/main/java/de/bytemc/cloud/plugin/CloudPlugin.java
@@ -3,9 +3,11 @@ package de.bytemc.cloud.plugin;
 import de.bytemc.cloud.api.CloudAPI;
 import de.bytemc.cloud.api.CloudAPITypes;
 import de.bytemc.cloud.api.groups.IGroupManager;
+import de.bytemc.cloud.api.logger.LogType;
 import de.bytemc.cloud.api.services.IServiceManager;
 import de.bytemc.cloud.plugin.console.DefaultCommandSender;
 import de.bytemc.cloud.plugin.groups.GroupManager;
+import de.bytemc.cloud.plugin.network.PluginClient;
 import de.bytemc.cloud.plugin.services.ServiceManager;
 import lombok.Getter;
 
@@ -15,11 +17,16 @@ public class CloudPlugin extends CloudAPI {
     private final DefaultCommandSender commandSender = new DefaultCommandSender();
     private final IGroupManager groupManager;
     private final IServiceManager serviceManager;
+    private final PluginClient pluginClient;
 
     public CloudPlugin() {
         super(CloudAPITypes.SERVICE);
 
         this.groupManager = new GroupManager();
         this.serviceManager = new ServiceManager();
+
+        this.pluginClient = new PluginClient("pxt-1", "127.0.0.1", 8876);
+
+        CloudAPI.getInstance().getLoggerProvider().logMessage("Successfully started plugin client.", LogType.SUCCESS);
     }
 }
