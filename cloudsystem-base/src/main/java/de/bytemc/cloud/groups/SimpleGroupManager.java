87a09f639a139b5c958ecc3f24edb1417c21b472
HttxMarco <mircolindenau04@gmail.com>
2022-01-25 13:02:04 +0100

    Load groups from database

--- cloudsystem-base/src/main/java/de/bytemc/cloud/groups/SimpleGroupManager.java
+++ cloudsystem-base/src/main/java/de/bytemc/cloud/groups/SimpleGroupManager.java
@@ -1,8 +1,18 @@
 package de.bytemc.cloud.groups;
 
+import de.bytemc.cloud.Base;
+import de.bytemc.cloud.api.CloudAPI;
 import de.bytemc.cloud.api.groups.impl.AbstractGroupManager;
 
+import java.util.stream.Collectors;
+
 public class SimpleGroupManager extends AbstractGroupManager {
+
+    public SimpleGroupManager(){
+        getAllCachedServiceGroups().addAll(Base.getInstance().getDatabaseManager().getDatabase().getAllServiceGroups());
+        CloudAPI.getInstance().getLoggerProvider().logMessage("§7Loading following groups: §b" +
+            String.join(", ", getAllCachedServiceGroups().stream().map(it -> it.getGroup()).collect(Collectors.joining())));
+    }
 
 
 
