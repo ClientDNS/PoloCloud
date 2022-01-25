d4c31ba5ad430909f2d79c54d3781b2288c0ecc9
HttxMarco <mircolindenau04@gmail.com>
2022-01-25 13:51:40 +0100

    Overrite add service methods, beacause database entry

--- cloudsystem-base/src/main/java/de/bytemc/cloud/groups/SimpleGroupManager.java
+++ cloudsystem-base/src/main/java/de/bytemc/cloud/groups/SimpleGroupManager.java
@@ -2,6 +2,7 @@ package de.bytemc.cloud.groups;
 
 import de.bytemc.cloud.Base;
 import de.bytemc.cloud.api.CloudAPI;
+import de.bytemc.cloud.api.groups.IServiceGroup;
 import de.bytemc.cloud.api.groups.impl.AbstractGroupManager;
 
 import java.util.stream.Collectors;
@@ -9,11 +10,14 @@ import java.util.stream.Collectors;
 public class SimpleGroupManager extends AbstractGroupManager {
 
     public SimpleGroupManager(){
-        getAllCachedServiceGroups().addAll(Base.getInstance().getDatabaseManager().getDatabase().getAllServiceGroups());
+        Base.getInstance().getDatabaseManager().getDatabase().getAllServiceGroups().forEach(it -> addServiceGroup(it));
         CloudAPI.getInstance().getLoggerProvider().logMessage("§7Loading following groups: §b" +
             String.join(", ", getAllCachedServiceGroups().stream().map(it -> it.getGroup()).collect(Collectors.joining())));
     }
 
-
-
+    @Override
+    public void addServiceGroup(IServiceGroup serviceGroup) {
+        Base.getInstance().getDatabaseManager().getDatabase().addGroup(serviceGroup);
+        super.addServiceGroup(serviceGroup);
+    }
 }
