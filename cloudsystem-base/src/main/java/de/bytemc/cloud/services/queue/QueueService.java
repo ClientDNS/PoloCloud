729e43b10f4e108fddc38c75ae15c2fc5fb8f22e
HttxMarco <mircolindenau04@gmail.com>
2022-01-26 10:10:39 +0100

    Rework on service queue

--- cloudsystem-base/src/main/java/de/bytemc/cloud/services/queue/QueueService.java
+++ cloudsystem-base/src/main/java/de/bytemc/cloud/services/queue/QueueService.java
@@ -1,6 +1,7 @@
 package de.bytemc.cloud.services.queue;
 
 import de.bytemc.cloud.api.CloudAPI;
+import de.bytemc.cloud.api.groups.IServiceGroup;
 import de.bytemc.cloud.api.services.IService;
 import de.bytemc.cloud.api.services.utils.ServiceState;
 
@@ -8,12 +9,18 @@ public class QueueService {
 
     private static final int MAX_BOOTABLE_SERVICES = 1;
 
+    public QueueService() {
+        CloudAPI.getInstance().getGroupManager().getAllCachedServiceGroups().stream().filter(it -> getAmountOfGroupServices(it) <= it.getMinOnlineService()).forEach(it -> {
+            //TODO PACKET & START
+            IService service = it.newService(1);
+            CloudAPI.getInstance().getLoggerProvider().logMessage("The group '§b" + it.getGroup() + "§7' start new instance of '§b" + service.getName() + "§7' (" + service.getServiceState().getName() + "§7)");
+        });
+        check();
+    }
+
     public void check() {
         if(minBootableServiceExists()) return;
         int bootable = MAX_BOOTABLE_SERVICES - getAmountOfBootableServices();
-
-
-
     }
 
 
@@ -24,6 +31,10 @@ public class QueueService {
 
     private int getAmountOfBootableServices(){
         return CloudAPI.getInstance().getServiceManager().getAllServicesByState(ServiceState.STARTING).size();
+    }
+
+    public int getAmountOfGroupServices(IServiceGroup serviceGroup) {
+        return (int) CloudAPI.getInstance().getServiceManager().getAllCachedServices().stream().filter(it -> it.getServiceGroup().equals(serviceGroup)).count();
     }
 
     public void startService(IService service) {
