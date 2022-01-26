024065d59ff3d245fd0e81e4c28d21bd1fce21f2
HttxMarco <mircolindenau04@gmail.com>
2022-01-26 13:39:55 +0100

    Add methode and remove useless constructor

--- cloudsystem-base/src/main/java/de/bytemc/cloud/services/ServiceManager.java
+++ cloudsystem-base/src/main/java/de/bytemc/cloud/services/ServiceManager.java
@@ -1,11 +1,21 @@
 package de.bytemc.cloud.services;
 
+import de.bytemc.cloud.api.CloudAPI;
+import de.bytemc.cloud.api.services.IService;
 import de.bytemc.cloud.api.services.impl.AbstractSimpleServiceManager;
+import de.bytemc.cloud.services.process.ProcessServiceStarter;
+import de.bytemc.network.promise.ICommunicationPromise;
 
 public class ServiceManager extends AbstractSimpleServiceManager {
 
-    public ServiceManager() {
+    public void start(IService service){
+        startService(service).addResultListener(it -> {
+            CloudAPI.getInstance().getLoggerProvider().logMessage("The service '§b" + service.getName() + "§7' selected and will now started. (" + service.getServiceState().getName() + "§7)");
+        }).addFailureListener(it -> it.printStackTrace());
+    }
 
+    public ICommunicationPromise<IService> startService(IService service){
+        return new ProcessServiceStarter(service).start();
     }
 
 }
