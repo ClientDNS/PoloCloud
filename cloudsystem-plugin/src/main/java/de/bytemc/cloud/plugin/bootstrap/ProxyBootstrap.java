a7a4c3c71f972433ff4c1e9346f63c5fda623e35
HttxMarco <mircolindenau04@gmail.com>
2022-01-26 14:01:45 +0100

    Bugfix cloud plugin

--- cloudsystem-plugin/src/main/java/de/bytemc/cloud/plugin/bootstrap/ProxyBootstrap.java
+++ cloudsystem-plugin/src/main/java/de/bytemc/cloud/plugin/bootstrap/ProxyBootstrap.java
@@ -1,12 +1,13 @@
 package de.bytemc.cloud.plugin.bootstrap;
 
+import de.bytemc.cloud.plugin.CloudPlugin;
 import net.md_5.bungee.api.plugin.Plugin;
 
 public class ProxyBootstrap extends Plugin {
 
     @Override
     public void onLoad() {
-
+        new CloudPlugin();
     }
 
     @Override
