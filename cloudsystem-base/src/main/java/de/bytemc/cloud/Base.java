27f53d689b6edcf9895370c1cae26ea2db244cf0
HttxMarco <mircolindenau04@gmail.com>
2022-01-26 09:34:55 +0100

    Opitimized shutdown methods with current promises

--- cloudsystem-base/src/main/java/de/bytemc/cloud/Base.java
+++ cloudsystem-base/src/main/java/de/bytemc/cloud/Base.java
@@ -1,5 +1,6 @@
 package de.bytemc.cloud;
 
+import com.google.common.collect.Lists;
 import de.bytemc.cloud.api.CloudAPI;
 import de.bytemc.cloud.api.CloudAPITypes;
 import de.bytemc.cloud.api.groups.IGroupManager;
@@ -12,6 +13,8 @@ import de.bytemc.cloud.groups.SimpleGroupManager;
 import de.bytemc.cloud.node.BaseNode;
 import de.bytemc.cloud.services.ServiceManager;
 import de.bytemc.cloud.services.queue.QueueService;
+import de.bytemc.network.promise.CommunicationPromise;
+import de.bytemc.network.promise.ICommunicationPromise;
 import lombok.Getter;
 
 @Getter
@@ -53,9 +56,10 @@ public class Base extends CloudAPI {
     }
 
     public void onShutdown() {
-        this.node.shutdownConnection();
-        this.databaseManager.shutdown();
-        System.exit(-1);
+        CloudAPI.getInstance().getLoggerProvider().logMessage("Trying to terminate cloudsystem.");
+        ICommunicationPromise.combineAll(Lists.newArrayList(node.shutdownConnection(), databaseManager.shutdown())).addCompleteListener(it -> System.exit(0)).addResultListener(unused -> {
+            CloudAPI.getInstance().getLoggerProvider().logMessage("Successfully shutdown the cloudsystem.", LogType.SUCCESS);
+        });
     }
 
 }
