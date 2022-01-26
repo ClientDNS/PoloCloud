b4e2b538da80493368a20c5aa97bbbe01f549b7d
HttxMarco <mircolindenau04@gmail.com>
2022-01-26 14:01:37 +0100

    Improve create service methode

--- cloudsystem-plugin/src/main/java/de/bytemc/cloud/plugin/services/ServiceManager.java
+++ cloudsystem-plugin/src/main/java/de/bytemc/cloud/plugin/services/ServiceManager.java
@@ -1,9 +1,15 @@
 package de.bytemc.cloud.plugin.services;
 
+import de.bytemc.cloud.api.services.IService;
 import de.bytemc.cloud.api.services.impl.AbstractSimpleServiceManager;
+import de.bytemc.network.promise.ICommunicationPromise;
 
 public class ServiceManager extends AbstractSimpleServiceManager {
 
 
-
+    @Override
+    public ICommunicationPromise<IService> startService(IService service) {
+        //TODO SEND PACKET
+        return null;
+    }
 }
