99a9dd314be0a08a554f270e228d797bd812d659
HttxMarco <mircolindenau04@gmail.com>
2022-01-27 12:36:06 +0100

    Add basic command alias

--- cloudsystem-api/src/main/java/de/bytemc/cloud/api/command/CloudCommand.java
+++ cloudsystem-api/src/main/java/de/bytemc/cloud/api/command/CloudCommand.java
@@ -5,11 +5,19 @@ import de.bytemc.cloud.api.command.executor.ICommandSender;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@AllArgsConstructor @Getter
+@Getter
 public abstract class CloudCommand {
 
     private String commandName, description;
+    private String[] alias;
     private ExecutorType executorType;
+
+    public CloudCommand(String commandName, String description, ExecutorType type, String... alias) {
+        this.commandName = commandName;
+        this.description = description;
+        this.executorType = type;
+        this.alias = alias;
+    }
 
     public abstract void execute(ICommandSender sender, String[] args);
 
