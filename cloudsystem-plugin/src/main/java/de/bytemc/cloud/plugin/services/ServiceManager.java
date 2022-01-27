4e67767facba7a13dc678819858060db90e98537
HttxMarco <mircolindenau04@gmail.com>
2022-01-27 12:39:29 +0100

    Register shutdown packet on plugin side

--- cloudsystem-plugin/src/main/java/de/bytemc/cloud/plugin/services/ServiceManager.java
+++ cloudsystem-plugin/src/main/java/de/bytemc/cloud/plugin/services/ServiceManager.java
@@ -1,11 +1,19 @@
 package de.bytemc.cloud.plugin.services;
 
+import de.bytemc.cloud.api.CloudAPI;
+import de.bytemc.cloud.api.network.packets.services.ServiceShutdownPacket;
 import de.bytemc.cloud.api.services.IService;
 import de.bytemc.cloud.api.services.impl.AbstractSimpleServiceManager;
+import de.bytemc.cloud.plugin.CloudPlugin;
 import de.bytemc.network.promise.ICommunicationPromise;
 
 public class ServiceManager extends AbstractSimpleServiceManager {
 
+    public ServiceManager(){
+        CloudAPI.getInstance().getNetworkHandler().registerPacketListener(ServiceShutdownPacket.class, (ctx, packet) -> {
+            CloudPlugin.getInstance().getPlugin().shutdown();
+        });
+    }
 
     @Override
     public ICommunicationPromise<IService> startService(IService service) {
