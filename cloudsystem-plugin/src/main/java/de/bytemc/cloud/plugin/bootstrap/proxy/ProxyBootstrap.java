4a930dffcc38c979d79345700eda7d6291384de2
HttxMarco <mircolindenau04@gmail.com>
2022-01-27 13:05:32 +0100

    Implement debug for proxy

--- cloudsystem-plugin/src/main/java/de/bytemc/cloud/plugin/bootstrap/proxy/ProxyBootstrap.java
+++ cloudsystem-plugin/src/main/java/de/bytemc/cloud/plugin/bootstrap/proxy/ProxyBootstrap.java
@@ -1,13 +1,30 @@
 package de.bytemc.cloud.plugin.bootstrap.proxy;
 
 import de.bytemc.cloud.plugin.CloudPlugin;
+import de.bytemc.cloud.plugin.IPlugin;
 import net.md_5.bungee.api.plugin.Plugin;
 
-public class ProxyBootstrap extends Plugin {
+import java.util.concurrent.TimeUnit;
+
+public class ProxyBootstrap extends Plugin implements IPlugin {
 
     @Override
     public void onLoad() {
-        new CloudPlugin();
+        new CloudPlugin(this);
     }
 
+    @Override
+    public void onEnable() {
+        System.out.println("onEnable");
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    @Override
+    public void shutdown() {
+        getProxy().getScheduler().schedule(this, () -> getProxy().stop(), 0, TimeUnit.MILLISECONDS);
+    }
 }
