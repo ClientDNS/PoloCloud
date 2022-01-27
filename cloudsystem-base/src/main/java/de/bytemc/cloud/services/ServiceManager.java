4e13dbb4b76d6a36ef9d1f9421a9f8ac003e11bb
HttxMarco <mircolindenau04@gmail.com>
2022-01-27 13:06:59 +0100

    Override shutdown service

--- cloudsystem-base/src/main/java/de/bytemc/cloud/services/ServiceManager.java
+++ cloudsystem-base/src/main/java/de/bytemc/cloud/services/ServiceManager.java
@@ -1,9 +1,13 @@
 package de.bytemc.cloud.services;
 
+import de.bytemc.cloud.Base;
 import de.bytemc.cloud.api.CloudAPI;
+import de.bytemc.cloud.api.network.packets.RedirectPacket;
+import de.bytemc.cloud.api.network.packets.services.ServiceShutdownPacket;
 import de.bytemc.cloud.api.services.IService;
 import de.bytemc.cloud.api.services.impl.AbstractSimpleServiceManager;
 import de.bytemc.cloud.services.process.ProcessServiceStarter;
+import de.bytemc.network.packets.IPacket;
 import de.bytemc.network.promise.ICommunicationPromise;
 
 public class ServiceManager extends AbstractSimpleServiceManager {
@@ -17,5 +21,18 @@ public class ServiceManager extends AbstractSimpleServiceManager {
     public ICommunicationPromise<IService> startService(IService service){
         return new ProcessServiceStarter(service).start();
     }
+
+    public void sendPacketToService(IService service, IPacket packet) {
+        Base.getInstance().getNode().getAllCachedConnectedClients().stream().filter(it -> it.getName().equals(service.getName())).findAny().ifPresent(it -> it.sendPacket(packet));
+    }
+
+    public void shutdownService(IService service){
+        if(service.getServiceGroup().getNode().equals(Base.getInstance().getNode().getNodeName())){
+            sendPacketToService(service, new ServiceShutdownPacket(service.getName()));
+        }else{
+            Base.getInstance().getNode().sendPacketToAllNodes(new RedirectPacket(service.getServiceGroup().getNode(),new ServiceShutdownPacket(service.getName())));
+        }
+    }
+
 
 }
