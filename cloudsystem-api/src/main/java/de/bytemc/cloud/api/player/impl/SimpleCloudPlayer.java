273ccf412023385767d8008b533be577668e2363
HttxMarco <mircolindenau04@gmail.com>
2022-01-28 12:02:42 +0100

    Implement cloud player logic

---
+++ cloudsystem-api/src/main/java/de/bytemc/cloud/api/player/impl/SimpleCloudPlayer.java
@@ -0,0 +1,20 @@
+package de.bytemc.cloud.api.player.impl;
+
+import de.bytemc.cloud.api.player.ICloudPlayer;
+import de.bytemc.cloud.api.services.IService;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Getter @Setter
+public class SimpleCloudPlayer implements ICloudPlayer {
+
+    private final UUID uniqueID;
+    private final String username;
+    private IService server;
+    private IService proxyServer;
+
+}
