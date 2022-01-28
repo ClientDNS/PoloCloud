8907afc032e52c0baa701d54564fad8d67401a69
HttxMarco <mircolindenau04@gmail.com>
2022-01-28 13:32:59 +0100

    Add dynamic service logger

--- cloudsystem-plugin/src/main/java/de/bytemc/cloud/plugin/CloudPlugin.java
+++ cloudsystem-plugin/src/main/java/de/bytemc/cloud/plugin/CloudPlugin.java
@@ -6,18 +6,19 @@ import de.bytemc.cloud.api.groups.IGroupManager;
 import de.bytemc.cloud.api.logger.LogType;
 import de.bytemc.cloud.api.player.ICloudPlayerManager;
 import de.bytemc.cloud.api.services.IServiceManager;
-import de.bytemc.cloud.plugin.console.DefaultCommandSender;
+import de.bytemc.cloud.plugin.console.DefaultProxyCommandSender;
 import de.bytemc.cloud.plugin.groups.GroupManager;
 import de.bytemc.cloud.plugin.network.PluginClient;
 import de.bytemc.cloud.plugin.player.CloudPlayerManager;
 import de.bytemc.cloud.plugin.services.ServiceManager;
 import de.bytemc.cloud.plugin.services.file.PluginPropertyFileReader;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 public class CloudPlugin extends CloudAPI {
 
-    private final DefaultCommandSender commandSender = new DefaultCommandSender();
+    @Setter private static DefaultProxyCommandSender commandSender;
 
     @Getter private static CloudPlugin instance;
 
@@ -42,5 +43,10 @@ public class CloudPlugin extends CloudAPI {
         pluginClient = new PluginClient(property.getService(), property.getHostname(), property.getPort());
 
         CloudAPI.getInstance().getLoggerProvider().logMessage("Successfully started plugin client.", LogType.SUCCESS);
+    }
+
+    @Override
+    public DefaultProxyCommandSender getCommandSender() {
+        return commandSender;
     }
 }
