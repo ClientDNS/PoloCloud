c55f21d98f55961ec5cfc0d6404f63871deccf13
HttxMarco <mircolindenau04@gmail.com>
2022-01-28 14:02:38 +0100

    Register new packets

--- cloudsystem-api/src/main/java/de/bytemc/cloud/api/network/impl/NetworkHandler.java
+++ cloudsystem-api/src/main/java/de/bytemc/cloud/api/network/impl/NetworkHandler.java
@@ -7,7 +7,9 @@ import de.bytemc.cloud.api.network.packets.group.ServiceGroupCacheUpdatePacket;
 import de.bytemc.cloud.api.network.packets.group.ServiceGroupExecutePacket;
 import de.bytemc.cloud.api.network.packets.player.CloudPlayerDisconnectPacket;
 import de.bytemc.cloud.api.network.packets.player.CloudPlayerLoginPacket;
+import de.bytemc.cloud.api.network.packets.services.ServiceAddPacket;
 import de.bytemc.cloud.api.network.packets.services.ServiceCacheUpdatePacket;
+import de.bytemc.cloud.api.network.packets.services.ServiceRemovePacket;
 import de.bytemc.cloud.api.network.packets.services.ServiceShutdownPacket;
 import de.bytemc.network.NetworkManager;
 import de.bytemc.network.packets.IPacket;
@@ -24,7 +26,9 @@ public class NetworkHandler implements INetworkHandler {
             ServiceCacheUpdatePacket.class,
             CloudQueryPacket.class,
             CloudPlayerLoginPacket.class,
-            CloudPlayerDisconnectPacket.class
+            CloudPlayerDisconnectPacket.class,
+            ServiceAddPacket.class,
+            ServiceRemovePacket.class
         );
 
     }
