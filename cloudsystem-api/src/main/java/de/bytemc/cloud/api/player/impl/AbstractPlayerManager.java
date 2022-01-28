add1d0068b8f050bc7a20f9b571a67ed443cc944
HttxMarco <mircolindenau04@gmail.com>
2022-01-28 12:02:29 +0100

    Add unregister and register function for player and getter methods for cloud player

--- cloudsystem-api/src/main/java/de/bytemc/cloud/api/player/impl/AbstractPlayerManager.java
+++ cloudsystem-api/src/main/java/de/bytemc/cloud/api/player/impl/AbstractPlayerManager.java
@@ -6,10 +6,24 @@ import de.bytemc.cloud.api.player.ICloudPlayerManager;
 import lombok.Getter;
 
 import java.util.List;
+import java.util.UUID;
 
 @Getter
 public abstract class AbstractPlayerManager implements ICloudPlayerManager {
 
     private final List<ICloudPlayer> allCachedCloudPlayers = Lists.newArrayList();
 
+    public abstract void registerCloudPlayer(UUID uniqueID, String username);
+
+    public abstract void unregisterCloudPlayer(UUID uuid);
+
+    @Override
+    public ICloudPlayer getCloudPlayerByNameOrNull(String username) {
+        return allCachedCloudPlayers.stream().filter(it -> it.getUsername().equalsIgnoreCase(username)).findAny().orElse(null);
+    }
+
+    @Override
+    public ICloudPlayer getCloudPlayerByUniqueIdOrNull(UUID uniqueID) {
+        return allCachedCloudPlayers.stream().filter(it -> it.getUniqueID().equals(uniqueID)).findAny().orElse(null);
+    }
 }
