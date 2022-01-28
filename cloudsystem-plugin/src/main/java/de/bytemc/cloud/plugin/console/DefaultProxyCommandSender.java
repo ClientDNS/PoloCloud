d051a99aa675f74095128a8174e59684f656a176
HttxMarco <mircolindenau04@gmail.com>
2022-01-28 13:39:59 +0100

    add default proxy logger

--- cloudsystem-plugin/src/main/java/de/bytemc/cloud/plugin/console/DefaultProxyCommandSender.java
+++ cloudsystem-plugin/src/main/java/de/bytemc/cloud/plugin/console/DefaultProxyCommandSender.java
@@ -2,12 +2,13 @@ package de.bytemc.cloud.plugin.console;
 
 import de.bytemc.cloud.api.command.executor.ExecutorType;
 import de.bytemc.cloud.api.command.executor.ICommandSender;
+import net.md_5.bungee.api.ProxyServer;
 
-public class DefaultCommandSender implements ICommandSender {
+public class DefaultProxyCommandSender implements ICommandSender {
 
     @Override
     public void sendMessage(String text) {
-        System.out.println(text);
+        ProxyServer.getInstance().getLogger().info(text);
     }
 
     @Override
