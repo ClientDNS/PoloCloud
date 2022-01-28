6f928ddbbbf4120ac5208ceb7701a4bcad76fdfc
HttxMarco <mircolindenau04@gmail.com>
2022-01-28 14:02:13 +0100

    Reformat code and listening on new packets

--- cloudsystem-base/src/main/java/de/bytemc/cloud/node/BaseNodeNetwork.java
+++ cloudsystem-base/src/main/java/de/bytemc/cloud/node/BaseNodeNetwork.java
@@ -2,25 +2,35 @@ package de.bytemc.cloud.node;
 
 import de.bytemc.cloud.Base;
 import de.bytemc.cloud.api.CloudAPI;
+import de.bytemc.cloud.api.network.INetworkHandler;
 import de.bytemc.cloud.api.network.packets.CloudQueryPacket;
 import de.bytemc.cloud.api.network.packets.RedirectPacket;
+import de.bytemc.cloud.api.network.packets.services.ServiceAddPacket;
+import de.bytemc.cloud.api.network.packets.services.ServiceRemovePacket;
+import de.bytemc.cloud.api.services.IServiceManager;
 import de.bytemc.network.NetworkManager;
 
 public class BaseNodeNetwork {
 
     public BaseNodeNetwork() {
-        CloudAPI.getInstance().getNetworkHandler().registerPacketListener(RedirectPacket.class, (ctx, packet) -> {
-            Base.getInstance().getNode().getAllCachedConnectedClients().stream().filter(it -> it.getName().equalsIgnoreCase(packet.getNode())).forEach(it -> {
-                it.sendPacket(packet.getPacket());
-            });
+        INetworkHandler networkHandler = CloudAPI.getInstance().getNetworkHandler();
+
+        networkHandler.registerPacketListener(RedirectPacket.class, (ctx, packet) ->
+            Base.getInstance().getNode().getAllCachedConnectedClients().stream().filter(it ->
+                it.getName().equalsIgnoreCase(packet.getNode())).forEach(it -> it.sendPacket(packet.getPacket())));
+
+        networkHandler.registerPacketListener(CloudQueryPacket.class, (ctx, packet) -> {
+            NetworkManager.callPacket(ctx, packet.getPacket());
+            Base.getInstance().getNode().getAllCachedConnectedClients().stream().filter(it ->
+                !it.getName().equalsIgnoreCase(packet.getIgnoredClients())).forEach(it ->
+                it.sendPacket(packet.getPacket()));
         });
 
-        CloudAPI.getInstance().getNetworkHandler().registerPacketListener(CloudQueryPacket.class, (ctx, packet) -> {
-            NetworkManager.callPacket(ctx, packet.getPacket());
-            Base.getInstance().getNode().getAllCachedConnectedClients().stream().filter(it -> !it.getName().equalsIgnoreCase(packet.getIgnoredClients())).forEach(it -> {
-                it.sendPacket(packet.getPacket());
-            });
-        });
+        IServiceManager serviceManager = CloudAPI.getInstance().getServiceManager();
+        networkHandler.registerPacketListener(ServiceRemovePacket.class, (ctx, packet) ->
+            serviceManager.getAllCachedServices().remove( serviceManager.getServiceByNameOrNull(packet.getService())));
+        networkHandler.registerPacketListener(ServiceAddPacket.class, (ctx, packet) -> serviceManager.getAllCachedServices().add(packet.getService()));
+
     }
 
 }
