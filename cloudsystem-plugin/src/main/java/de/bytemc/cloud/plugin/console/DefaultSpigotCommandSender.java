30028f76b9e9208fc2ac24d3cb7b9841eac939dd
HttxMarco <mircolindenau04@gmail.com>
2022-01-28 13:39:51 +0100

    Add default spigot logger

---
+++ cloudsystem-plugin/src/main/java/de/bytemc/cloud/plugin/console/DefaultSpigotCommandSender.java
@@ -0,0 +1,12 @@
+package de.bytemc.cloud.plugin.console;
+
+import de.bytemc.cloud.plugin.bootstrap.spigot.SpigotBootstrap;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DefaultSpigotCommandSender extends DefaultProxyCommandSender {
+
+    @Override
+    public void sendMessage(String text) {
+        JavaPlugin.getPlugin(SpigotBootstrap.class).getLogger().info(text);
+    }
+}
