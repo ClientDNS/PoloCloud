9e27aae1767ecb0a777de820a234c9fc8ea4de69
HttxMarco <mircolindenau04@gmail.com>
2022-01-28 12:04:03 +0100

    Implement unregister and register player methods

--- cloudsystem-base/src/main/java/de/bytemc/cloud/player/CloudPlayerManager.java
+++ cloudsystem-base/src/main/java/de/bytemc/cloud/player/CloudPlayerManager.java
@@ -2,13 +2,28 @@ package de.bytemc.cloud.player;
 
 import de.bytemc.cloud.api.player.ICloudPlayer;
 import de.bytemc.cloud.api.player.impl.AbstractPlayerManager;
+import de.bytemc.cloud.api.player.impl.SimpleCloudPlayer;
 
 import java.util.List;
+import java.util.UUID;
 
 public class CloudPlayerManager extends AbstractPlayerManager {
 
     @Override
     public List<ICloudPlayer> getAllServicePlayers() {
         return getAllCachedCloudPlayers();
+    }
+
+
+    @Override
+    public void registerCloudPlayer(UUID uniqueID, String username) {
+        //send packet to node
+        getAllServicePlayers().add(new SimpleCloudPlayer(uniqueID, username));
+    }
+
+    @Override
+    public void unregisterCloudPlayer(UUID uuid) {
+        //send packet to node
+
     }
 }
--- cloudsystem-plugin/src/main/java/de/bytemc/cloud/plugin/player/CloudPlayerManager.java
+++ cloudsystem-plugin/src/main/java/de/bytemc/cloud/plugin/player/CloudPlayerManager.java
@@ -6,6 +6,7 @@ import de.bytemc.cloud.plugin.CloudPlugin;
 import de.bytemc.cloud.plugin.services.ServiceManager;
 
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 public class CloudPlayerManager extends AbstractPlayerManager {
@@ -13,5 +14,15 @@ public class CloudPlayerManager extends AbstractPlayerManager {
     @Override
     public List<ICloudPlayer> getAllServicePlayers() {
         return getAllServicePlayers().stream().filter(it -> it.getServer().getName().equalsIgnoreCase(((ServiceManager)CloudPlugin.getInstance().getServiceManager()).thisService().getName())).collect(Collectors.toList());
+    }
+
+    @Override
+    public void registerCloudPlayer(UUID uniqueID, String username) {
+        //TODO
+    }
+
+    @Override
+    public void unregisterCloudPlayer(UUID uuid) {
+        //TODO
     }
 }
