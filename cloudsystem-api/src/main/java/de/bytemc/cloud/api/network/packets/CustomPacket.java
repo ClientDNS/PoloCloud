39f589991adc067b3c042631339c37113c17cfbe
RealBauHD <bauhd@gmx.net>
2022-02-08 19:04:06 +0100

    Added CustomPacket

---
+++ cloudsystem-api/src/main/java/de/bytemc/cloud/api/network/packets/CustomPacket.java
@@ -0,0 +1,29 @@
+package de.bytemc.cloud.api.network.packets;
+
+import de.bytemc.cloud.api.json.Document;
+import de.bytemc.network.packets.IPacket;
+import de.bytemc.network.packets.NetworkByteBuf;
+import io.netty.buffer.ByteBuf;
+import org.jetbrains.annotations.NotNull;
+
+public final class CustomPacket implements IPacket {
+
+    private Document document;
+
+    public CustomPacket() {}
+
+    public CustomPacket(final @NotNull Document document) {
+        this.document = document;
+    }
+
+    @Override
+    public void write(NetworkByteBuf networkByteBuf) {
+        networkByteBuf.writeString(this.document.getJsonObject().toString());
+    }
+
+    @Override
+    public void read(NetworkByteBuf networkByteBuf) {
+        this.document = new Document(networkByteBuf.readString());
+    }
+
+}
