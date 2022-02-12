004779892237e6506c30a49b1baa33b020e0c848
RealBauHD <bauhd@gmx.net>
2022-02-12 16:41:28 +0100

    base - optimizing

--- cloudsystem-base/src/main/java/de/bytemc/cloud/command/impl/ServiceCloudCommand.java
+++ cloudsystem-base/src/main/java/de/bytemc/cloud/command/impl/ServiceCloudCommand.java
@@ -11,62 +11,52 @@ import de.bytemc.cloud.services.ServiceManager;
 
 public final class ServiceCloudCommand extends CloudCommand {
 
+    private final CloudAPI cloudAPI;
+
     public ServiceCloudCommand() {
         super("service", "Manage services", ExecutorType.CONSOLE, "ser");
+        this.cloudAPI = CloudAPI.getInstance();
     }
 
     @Override
     public void execute(ICommandSender sender, String[] args) {
-
-        var log = CloudAPI.getInstance().getLoggerProvider();
+        final var log = this.cloudAPI.getLoggerProvider();
 
         if (args.length == 1 && args[0].equalsIgnoreCase("list")) {
-            for (IService service : CloudAPI.getInstance().getServiceManager().getAllCachedServices()) {
-                log.logMessage("Name of service '§b" + service.getName() + "§7' (§7State of service '§b" + service.getServiceState().getName() + "§7' | Node: '" + service.getServiceGroup().getNode() + "')");
+            for (final IService service : this.cloudAPI.getServiceManager().getAllCachedServices()) {
+                log.logMessage("Name of service '§b" + service.getName()
+                    + "§7' (§7State of service '§b" + service.getServiceState().getName()
+                    + "§7' | Node: '" + service.getServiceGroup().getNode() + "')");
             }
             return;
-        }
-        if (args.length == 2 && args[0].equalsIgnoreCase("stop")) {
+        } else if (args.length == 2 && args[0].equalsIgnoreCase("stop")) {
+            this.cloudAPI.getServiceManager().getService(args[1]).ifPresentOrElse(service -> {
+                if (service.getServiceState() == ServiceState.PREPARED || service.getServiceState() == ServiceState.STOPPING) {
+                    log.logMessage("This service ist not started or already in stopping state.", LogType.WARNING);
+                    return;
+                }
 
-            IService service = CloudAPI.getInstance().getServiceManager().getServiceByNameOrNull(args[1]);
-
-            if (service == null) {
-                log.logMessage("This service does not exists.", LogType.WARNING);
-                return;
-            }
-
-            if (service.getServiceState() == ServiceState.PREPARED || service.getServiceState() == ServiceState.STOPPING) {
-                log.logMessage("This service ist not started or already in stopping state.", LogType.WARNING);
-                return;
-            }
-
-            ((ServiceManager) CloudAPI.getInstance().getServiceManager()).shutdownService(service);
-            log.logMessage("The service '§b" + service.getName() + "§7' is now stopped.");
+                ((ServiceManager) this.cloudAPI.getServiceManager()).shutdownService(service);
+                log.logMessage("The service '§b" + service.getName() + "§7' is now stopped.");
+            }, () -> log.logMessage("This service does not exists.", LogType.WARNING));
             return;
-        }
-        if (args.length == 4) {
+        } else if (args.length == 4) {
             if (args[0].equalsIgnoreCase("start")) {
-                String serviceName = args[1];
+                // TODO
             }
             return;
-        }
-        if (args.length == 2 && args[0].equalsIgnoreCase("info")) {
-
-            IService service = CloudAPI.getInstance().getServiceManager().getServiceByNameOrNull(args[1]);
-
-            if (service == null) {
-                log.logMessage("This service does not exists.", LogType.WARNING);
-                return;
-            }
-
-            log.logMessage("Service information:");
-            log.logMessage("Name: §b" + service.getName());
-            log.logMessage("ID: §b" + service.getServiceID());
-            log.logMessage("Group: §b" + service.getServiceGroup().getName());
-            log.logMessage("Host: §b" + service.getHostName());
-            log.logMessage("Port: §b" + service.getPort());
+        } else if (args.length == 2 && args[0].equalsIgnoreCase("info")) {
+            this.cloudAPI.getServiceManager().getService(args[1]).ifPresentOrElse(service -> {
+                log.logMessage("Service information:");
+                log.logMessage("Name: §b" + service.getName());
+                log.logMessage("ID: §b" + service.getServiceID());
+                log.logMessage("Group: §b" + service.getServiceGroup().getName());
+                log.logMessage("Host: §b" + service.getHostName());
+                log.logMessage("Port: §b" + service.getPort());
+            }, () -> log.logMessage("The service does not exists.", LogType.WARNING));
             return;
         }
+
         log.logMessage("§7Use following command: §bservice list §7- List all available services.");
         log.logMessage("§7Use following command: §bservice start (name) §7- Starting a specific service that exists.");
         log.logMessage("§7Use following command: §bservice stop (name) §7- Stopping a specific service that exists.");
