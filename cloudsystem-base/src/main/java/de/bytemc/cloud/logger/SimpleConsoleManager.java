d73ebea0e573e3c69fade85f1da50506b8706236
RealBauHD <bauhd@gmx.net>
2022-02-16 14:28:24 +0100

    base - removed DefaultLineReader.java

--- cloudsystem-base/src/main/java/de/bytemc/cloud/logger/SimpleConsoleManager.java
+++ cloudsystem-base/src/main/java/de/bytemc/cloud/logger/SimpleConsoleManager.java
@@ -1,15 +1,18 @@
 package de.bytemc.cloud.logger;
 
+import com.google.common.base.Charsets;
 import com.google.common.base.Throwables;
 import de.bytemc.cloud.Base;
 import de.bytemc.cloud.api.CloudAPI;
 import de.bytemc.cloud.api.logger.LogType;
 import de.bytemc.cloud.api.logger.Logger;
-import de.bytemc.cloud.logger.builder.DefaultLineReader;
+import de.bytemc.cloud.logger.complete.ConsoleAutoCompleteTool;
 import de.bytemc.cloud.logger.runnable.ConsoleReadingThread;
 import lombok.Getter;
 import lombok.SneakyThrows;
 import org.jline.reader.LineReader;
+import org.jline.reader.LineReaderBuilder;
+import org.jline.terminal.TerminalBuilder;
 
 @Getter
 public final class SimpleConsoleManager {
@@ -19,9 +22,16 @@ public final class SimpleConsoleManager {
     private final LineReader lineReader;
     private final boolean windowsSystem;
 
+    @SneakyThrows
     public SimpleConsoleManager(final Logger loggerProvider) {
         this.windowsSystem = ((SimpleLogger) loggerProvider).isWindows();
-        this.lineReader = DefaultLineReader.read().preparedTerminal().addOptions().complete();
+        this.lineReader = LineReaderBuilder.builder()
+            .terminal(TerminalBuilder.builder().system(true).streams(System.in, System.out).encoding(Charsets.UTF_8).dumb(true).build())
+            .option(LineReader.Option.DISABLE_EVENT_EXPANSION, true)
+            .option(LineReader.Option.AUTO_REMOVE_SLASH, false)
+            .option(LineReader.Option.INSERT_TAB, false)
+            .completer(new ConsoleAutoCompleteTool())
+            .build();
         this.loggerProvider = loggerProvider;
 
         this.loggerProvider.clearConsole();
