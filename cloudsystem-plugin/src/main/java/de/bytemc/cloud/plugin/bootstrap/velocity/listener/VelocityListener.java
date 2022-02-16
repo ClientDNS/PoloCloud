c18d676a429e04da302305560b8ab19180119414
RealBauHD <bauhd@gmx.net>
2022-02-16 20:01:19 +0100

    base - Velocity add ServerConnectedEvent

--- cloudsystem-plugin/src/main/java/de/bytemc/cloud/plugin/bootstrap/velocity/listener/VelocityListener.java
+++ cloudsystem-plugin/src/main/java/de/bytemc/cloud/plugin/bootstrap/velocity/listener/VelocityListener.java
@@ -3,12 +3,17 @@ package de.bytemc.cloud.plugin.bootstrap.velocity.listener;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.connection.DisconnectEvent;
 import com.velocitypowered.api.event.connection.LoginEvent;
+import com.velocitypowered.api.event.player.ServerConnectedEvent;
 import com.velocitypowered.api.event.player.ServerPreConnectEvent;
 import com.velocitypowered.api.event.proxy.ProxyPingEvent;
 import com.velocitypowered.api.proxy.ProxyServer;
+import de.bytemc.cloud.api.CloudAPI;
+import de.bytemc.cloud.api.events.events.CloudPlayerUpdateEvent;
 import de.bytemc.cloud.api.player.impl.SimpleCloudPlayer;
 import de.bytemc.cloud.plugin.bootstrap.velocity.VelocityBootstrap;
 import de.bytemc.cloud.wrapper.Wrapper;
+
+import java.util.Objects;
 
 public record VelocityListener(VelocityBootstrap bootstrap, ProxyServer proxyServer) {
 
@@ -28,6 +33,19 @@ public record VelocityListener(VelocityBootstrap bootstrap, ProxyServer proxySer
                     registeredServer -> event.setResult(ServerPreConnectEvent.ServerResult.allowed(registeredServer)),
                     () -> event.setResult(ServerPreConnectEvent.ServerResult.denied()));
         }
+    }
+
+    @Subscribe
+    public void handle(final ServerConnectedEvent event) {
+        final var player = event.getPlayer();
+
+        player.getCurrentServer().ifPresent(serverConnection ->
+            CloudAPI.getInstance().getCloudPlayerManager().getCloudPlayer(player.getUniqueId())
+                .ifPresent(cloudPlayer -> {
+                    cloudPlayer.setServer(Objects.requireNonNull(CloudAPI.getInstance().getServiceManager()
+                        .getServiceByNameOrNull(serverConnection.getServerInfo().getName())));
+                    cloudPlayer.update(CloudPlayerUpdateEvent.UpdateReason.SERVER_SWITCH);
+                }));
     }
 
     @Subscribe
