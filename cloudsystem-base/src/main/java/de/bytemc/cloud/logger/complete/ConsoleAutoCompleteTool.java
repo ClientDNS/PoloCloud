d2ea07d426ccd47ebb0c40628707b6624470f213
RealBauHD <bauhd@gmx.net>
2022-02-16 12:51:25 +0100

    base - fix auto complete

--- cloudsystem-base/src/main/java/de/bytemc/cloud/logger/complete/ConsoleAutoCompleteTool.java
+++ cloudsystem-base/src/main/java/de/bytemc/cloud/logger/complete/ConsoleAutoCompleteTool.java
@@ -1,24 +1,20 @@
 package de.bytemc.cloud.logger.complete;
 
-import com.google.common.collect.Lists;
 import de.bytemc.cloud.Base;
 import org.jline.reader.Candidate;
 import org.jline.reader.Completer;
 import org.jline.reader.LineReader;
 import org.jline.reader.ParsedLine;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public final class ConsoleAutoCompleteTool implements Completer {
 
     private List<String> getInputConsoleSuggestions(final String input) {
+        var arguments = input.split(" ");
         if (input.isEmpty() || input.indexOf(' ') == -1) {
             final Collection<String> registeredCommands = Base.getInstance().getCommandManager().getCachedCloudCommands().keySet();
-            final var splitInput = input.split(" ");
-            final var toTest = splitInput[splitInput.length - 1];
+            final var toTest = arguments[arguments.length - 1];
             final List<String> result = new LinkedList<>();
             for (final String s : registeredCommands) {
                 if (s != null && (toTest.trim().isEmpty() || s.toLowerCase().contains(toTest.toLowerCase()))) {
@@ -32,11 +28,17 @@ public final class ConsoleAutoCompleteTool implements Completer {
 
             return result;
         } else {
-            final var arguments = Lists.newArrayList(input.split(" "));
-            final var command = Base.getInstance().getCommandManager().getCachedCloudCommands().get(arguments.get(0));
+            final var command = Base.getInstance().getCommandManager().getCachedCloudCommands().get(arguments[0]);
+            if (arguments.length > 1) {
+                if (input.endsWith(" ")) {
+                    arguments = Arrays.copyOfRange(arguments, 1, arguments.length + 1);
+                    arguments[arguments.length - 1] = "";
+                } else {
+                    arguments = Arrays.copyOfRange(arguments, 1, arguments.length);
+                }
+            }
             if (command != null) {
-                arguments.remove(0);
-                return command.tabComplete(arguments.toArray(new String[]{}));
+                return command.tabComplete(arguments);
             }
         }
         return new ArrayList<>();
@@ -46,10 +48,10 @@ public final class ConsoleAutoCompleteTool implements Completer {
     public void complete(LineReader lineReader, ParsedLine parsedLine, List<Candidate> list) {
         final String inputLine = parsedLine.line();
 
-        final List<String> suggestions = new ArrayList<>(this.getInputConsoleSuggestions(inputLine));
+        final List<String> suggestions = this.getInputConsoleSuggestions(inputLine);
         if (suggestions.isEmpty()) return;
 
-        list.addAll(suggestions.stream().map(Candidate::new).toList());
+        suggestions.stream().map(Candidate::new).forEach(list::add);
     }
 
 }
