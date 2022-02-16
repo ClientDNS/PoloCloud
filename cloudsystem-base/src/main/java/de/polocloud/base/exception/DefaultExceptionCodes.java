32b6447e2e0e9192538d081529666a968607c3f9
RealBauHD <bauhd@gmx.net>
2022-02-17 00:59:24 +0100

    base - logger is not null and print stack trace

--- cloudsystem-base/src/main/java/de/polocloud/base/exception/DefaultExceptionCodes.java
+++ cloudsystem-base/src/main/java/de/polocloud/base/exception/DefaultExceptionCodes.java
@@ -11,18 +11,14 @@ public class DefaultExceptionCodes {
     public DefaultExceptionCodes() {
         ErrorHandler.defaultInstance()
             .onError(SQLException.class, ((throwable, errorHandler) -> {
-                if (CloudAPI.getInstance().getLogger() == null) {
-                    System.err.println("SQLError occurred, check your database credentials! (" + throwable.getMessage() + ")");
-                    return;
-                }
-                CloudAPI.getInstance().getLogger().log("§cSQLError occurred§7, check your §bDatabase credentials! §7(§b" + throwable.getMessage() + "§7)", LogType.ERROR);
+                CloudAPI.getInstance().getLogger()
+                    .log("§cSQLError occurred§7, check your §bDatabase credentials! §7(§b" + throwable.getMessage() + "§7)", LogType.ERROR);
+                throwable.printStackTrace();
             }))
             .bindCode(1, errorCode -> throwable -> throwable instanceof RuntimeException, ((throwable, errorHandler) -> {
-                if (CloudAPI.getInstance().getLogger() == null) {
-                    System.err.println("Fatal exception occurred, stopping... (" + throwable.getMessage() + ")");
-                    return;
-                }
-                CloudAPI.getInstance().getLogger().log("§cFatal exception occurred§7, cloud will try to stop... (§b" + throwable.getMessage() + "§7)", LogType.ERROR);
+                CloudAPI.getInstance().getLogger()
+                    .log("§cFatal exception occurred§7, cloud will try to stop... (§b" + throwable.getMessage() + "§7)", LogType.ERROR);
+                throwable.printStackTrace();
                 Base.getInstance().onShutdown();
             }));
     }
