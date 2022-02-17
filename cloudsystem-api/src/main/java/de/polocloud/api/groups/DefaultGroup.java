f2aa42b4f07f90f6c9a202fb3fd3d83b12a27627
RealBauHD <bauhd@gmx.net>
2022-02-17 02:26:46 +0100

    api - set fallback default false

--- cloudsystem-api/src/main/java/de/polocloud/api/groups/DefaultGroup.java
+++ cloudsystem-api/src/main/java/de/polocloud/api/groups/DefaultGroup.java
@@ -7,7 +7,7 @@ public final class DefaultGroup extends ServiceGroup {
 
     public DefaultGroup(String group, int memory, boolean staticService, GameServerVersion gameServerVersion) {
         super(group, group, "node-1", "A default service", memory, 100, 1,
-            -1, staticService, true, false, true, gameServerVersion);
+            -1, staticService, false, false, true, gameServerVersion);
     }
 
 }
