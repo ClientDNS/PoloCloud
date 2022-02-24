c6bd556531efadfc5ae0387c11604d48d316729c
RealBauHD <bauhd@gmx.net>
2022-02-24 17:47:29 +0100

    base - fix config reading

--- cloudsystem-base/src/main/java/de/polocloud/base/config/CloudConfiguration.java
+++ cloudsystem-base/src/main/java/de/polocloud/base/config/CloudConfiguration.java
@@ -1,21 +1,25 @@
 package de.polocloud.base.config;
 
 import de.polocloud.database.DatabaseConfiguration;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
-@AllArgsConstructor
 public class CloudConfiguration {
 
-    private final NodeConfiguration nodeConfiguration = new NodeConfiguration("Node-1", "127.0.0.1", 8876);
-    private final List<NodeConfiguration> connectedNodes = new ArrayList<>();
+    private final NodeConfiguration nodeConfiguration;
+    private final List<NodeConfiguration> connectedNodes;
+    private final DatabaseConfiguration databaseConfiguration;
+    private final String javaCommand;
 
-    private final DatabaseConfiguration databaseConfiguration = new DatabaseConfiguration();
-
-    private final String javaCommand = "java";
+    // default parameters
+    public CloudConfiguration() {
+        this.nodeConfiguration = new NodeConfiguration("Node-1", "127.0.0.1", 8876);
+        this.connectedNodes = new ArrayList<>();
+        this.databaseConfiguration = new DatabaseConfiguration();
+        this.javaCommand = "java";
+    }
 
 }
--- cloudsystem-database/src/main/java/de/polocloud/database/DatabaseConfiguration.java
+++ cloudsystem-database/src/main/java/de/polocloud/database/DatabaseConfiguration.java
@@ -5,13 +5,23 @@ import lombok.Getter;
 @Getter
 public class DatabaseConfiguration {
 
-    private final String hostname = "localhost";
-    private final String database = "polocloud";
-    private final String username = "admin";
-    private final String password = "password";
+    private final String hostname;
+    private final String database;
+    private final String username;
+    private final String password;
 
-    private final int port = 3306;
+    private final int port;
 
-    private final DatabaseType databaseType = DatabaseType.MYSQL;
+    private final DatabaseType databaseType;
+
+    // default parameters
+    public DatabaseConfiguration() {
+        this.hostname = "127.0.0.1";
+        this.database = "cloud";
+        this.username = "admin";
+        this.password = "password";
+        this.port = 3306;
+        this.databaseType = DatabaseType.MYSQL;
+    }
 
 }
