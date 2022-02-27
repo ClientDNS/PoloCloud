92c6f4386f1ab25bfd51f84ddc0822e43997bdd8
iPommes <pommes2303@gmail.com>
2022-02-27 17:02:54 +0100

    Reworked and Fixed Stuff - Reworked the Group Command, now it follows the scheme: "group (name) edit/info/remove" - Fixed the Used Memory in the InfoCommand

--- cloudsystem-base/src/main/java/de/polocloud/base/command/defaults/InfoCommand.java
+++ cloudsystem-base/src/main/java/de/polocloud/base/command/defaults/InfoCommand.java
@@ -2,7 +2,6 @@ package de.polocloud.base.command.defaults;
 
 import de.polocloud.base.Base;
 import de.polocloud.base.command.CloudCommand;
-import org.jetbrains.annotations.NotNull;
 
 @CloudCommand.Command(name = "info", description = "Prints information about the cloud")
 public final class InfoCommand extends CloudCommand {
@@ -14,10 +13,10 @@ public final class InfoCommand extends CloudCommand {
         base.getLogger().log("§7Version: §b" + Base.getInstance().getVersion(),
             "§7Node: §b" + Base.getInstance().getNode().getName(),
             "§7Threads: §b" + Thread.getAllStackTraces().keySet().size(),
-            "§7RAM: §b" + ((runtime.totalMemory() - calcMemory(runtime.freeMemory()) + "/" + calcMemory(runtime.maxMemory()) + "mb")));
+            "§7RAM: §b" + calcMemory(runtime.totalMemory() - runtime.freeMemory()) + "/" + calcMemory(runtime.maxMemory()) + "mb");
     }
 
-    private long calcMemory(final @NotNull long memory){
+    private long calcMemory(final long memory) {
         return memory / 1024 / 1024;
     }
 
