924e51b57fabbf39b2cb83edb189a3d7067042f3
BauHD <71191102+RealBauHD@users.noreply.github.com>
2022-03-28 19:10:21 +0200

    [skip ci] Update Transformer.java

--- cloudsystem-wrapper/src/main/java/de/polocloud/wrapper/transformer/Transformer.java
+++ cloudsystem-wrapper/src/main/java/de/polocloud/wrapper/transformer/Transformer.java
@@ -5,6 +5,10 @@ import org.objectweb.asm.tree.ClassNode;
 
 public interface Transformer {
 
+    /**
+     * credits to @CloudNetServices
+     * GitHub: https://github.com/CloudNetService/CloudNet-v3
+     */
     void transform(@NotNull String className, @NotNull ClassNode classNode);
 
 }
